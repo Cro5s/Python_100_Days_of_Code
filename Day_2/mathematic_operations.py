793036355682a0df3d4b5b4f28aa9667c60d236b
Kadeem Jackson <kdjackson35@gmail.com>
2021-01-12 00:57:00 -0500

    Add Mathematic Operations

---
+++ Day_2/mathematic_operations.py
@@ -0,0 +1,13 @@
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+# PEMDAS Left to Right
+# Parenthesis ()
+# Exponent **
+# Multiplication * Same priority as division but will execute from left to right
+# Division / Same priority as multiplication but will execute from left to right
+# Addition + Same priority as subtraction but will execute from left to right
+# Subtraction - Same priority as addition but will execute from left to right
