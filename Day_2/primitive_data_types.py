d044b9561405aa592a50a9731b9ef7f997eb3ba4
Kadeem Jackson <kdjackson35@gmail.com>
2021-01-11 23:59:55 -0500

    Add primitive data types

---
+++ Day_2/primitive_data_types.py
@@ -0,0 +1,20 @@
+# Data Types
+
+# String
+# Allows you to print a specific character at an index
+print ("Hello"[0])
+
+# Integer
+print(123 + 456)
+# The underscores are removed from integers
+print(123_456_789)
+print(123456789)
+print(123456789 == 123_456_789)
+
+# Float
+3.14159
+
+# Boolean
+# Always uses Capitalized
+True
+False
