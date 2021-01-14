3c75caed03d22eda010dfacb01ff3c72ea2a92ad
Kadeem Jackson <kdjackson35@gmail.com>
2021-01-14 17:43:49 -0500

    Add number manipulation and f-String

---
+++ Day_2/number_manipulation.py
@@ -0,0 +1,13 @@
+print(8 / 3) # Prints the result as a floating number
+print(round(8 / 3)) # Rounds the number and turns it into an integer
+print(round(8 / 3, 2)) # Rounds the number to 2 decimal places
+print(8 // 3) # Performs floor division which returns a int instead of a float
+results = 4 / 2
+results /= 2
+print(results) 
+
+# f-String turns all input into a string
+score = 0
+height = 1.8
+isWinning = True
+print(f"Your score is {score}, your height is {height}, and you are winning is {isWinning}")
