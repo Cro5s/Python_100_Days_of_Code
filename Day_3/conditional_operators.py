64c53cec5b5b008a164ebccc9d00166063359a89
Kadeem Jackson <kdjackson35@gmail.com>
2021-01-15 14:38:37 -0500

    Add conditional operators for day 3

---
+++ Day_3/conditional_operators.py
@@ -0,0 +1,19 @@
+# For if statements you can choose whether or not you want to use ()
+# End a statement line with :
+# You have to indent a block of code that is inside a function/statement
+
+# Comparison Operators
+#   >   Greater than
+#   <   Less than
+#   >=  Greater than or equal to
+#   <=  Less than or equal to
+#   ==  Equal to
+#   !=  Not equal to
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+else:
+  print("Sorry, you have to grow taller before you can ride.")
