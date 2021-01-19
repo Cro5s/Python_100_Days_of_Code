225f4a89f8690377a165b24e2efeab4d1f2c04b8
Kadeem Jackson <kdjackson35@gmail.com>
2021-01-18 21:44:00 -0500

    Complete Love Calculator

--- Day_3/love_calculator.py
+++ Day_3/love_calculator.py
@@ -5,41 +5,29 @@
 # Take both people's names and check for the number of times the letters in the word TRUE occurs. Then check for the number of times the letters in the word LOVE occurs. Then combine these numbers to make a 2 digit number.
 
 # For Love Scores less than 10 or greater than 90, the message should be:
-
 # "Your score is **x**, you go together like coke and mentos."
 
 # For Love Scores between 40 and 50, the message should be:
-
 # "Your score is **y**, you are alright together."
 
 # Otherwise, the message will just be their score. e.g.:
-
 # "Your score is **z**."
 
 # e.g.
 
 # name1 = "Angela Yu"
-
 # name2 = "Jack Bauer"
 
 # T occurs 0 times
-
 # R occurs 1 time
-
 # U occurs 2 times
-
 # E occurs 2 times
-
 # Total = 5
 
 # L occurs 1 time
-
 # O occurs 0 times
-
 # V occurs 0 times
-
 # E occurs 2 times
-
 # Total = 3
 
 # Love Score = 53
@@ -68,6 +56,7 @@
 # "Your score is 47, you are alright together."
 # "Your score is 125, you go together like coke and mentos."
 # "Your score is 54."
+
 # Hint
 # The lower() function changes all the letters in a string to lower case.
 # https://stackoverflow.com/questions/6797984/how-do-i-lowercase-a-string-in-python
@@ -82,3 +71,32 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+trueCount = 0
+loveCount = 0
+
+trueCount += name1.lower().count("t")
+trueCount += name2.lower().count("t")
+trueCount += name1.lower().count("r")
+trueCount += name2.lower().count("r")
+trueCount += name1.lower().count("u")
+trueCount += name2.lower().count("u")
+trueCount += name1.lower().count("e")
+trueCount += name2.lower().count("e")
+
+loveCount += name1.lower().count("l")
+loveCount += name2.lower().count("l")
+loveCount += name1.lower().count("o")
+loveCount += name2.lower().count("o")
+loveCount += name1.lower().count("v")
+loveCount += name2.lower().count("v")
+loveCount += name1.lower().count("e")
+loveCount += name2.lower().count("e")
+
+score = str(trueCount) + str(loveCount)
+
+if int(score) < 10 or int(score) > 90:
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif int(score) > 40 and int(score) < 50:
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
